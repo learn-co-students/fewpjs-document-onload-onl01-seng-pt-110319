e1aa5a05e2383fef8214fabacdeab3f7a6467429
Aida Abdikulova <aabdikul@Aidas-MBP.fios-router.home>
2020-05-29 01:43:14 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+  let str = document.getElementById("text")
+  document.getElementById("text").innerHTML = text.innerHTML.replace(text.innerHTML, "This is really cool!")
+});
