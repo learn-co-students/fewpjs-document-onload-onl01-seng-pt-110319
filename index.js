0963d9531f6e78863a6740d5ab2fd68ad87683a7
jhar3ll <jharell.chambers@selu.edu>
2020-06-07 16:31:32 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Your code goes here
+
+  
+document.addEventListener( "DOMContentLoaded", function () {
+    domLoaded()
+  } );
+  
+  function domLoaded() {
+    document.getElementById("text")
+      .textContent = "This is really cool!";
+  }
